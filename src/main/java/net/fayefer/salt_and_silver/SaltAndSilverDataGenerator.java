b01ad3dc46109e108413ff319bb490af66eb78be
fayefer <fayeefer@gmail.com>
2024-10-19 22:52:44 -0300

    Initial commit

---
+++ src/main/java/net/fayefer/salt_and_silver/SaltAndSilverDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fayefer.salt_and_silver;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SaltAndSilverDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
